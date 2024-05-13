b836831164804f8cc830a75b0955c62a522ad6b4
Simon Gustavsson <simgus1234@gmail.com>
2024-05-13 17:20:13 +0200

    Added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 import Navbar from "./components/Navbar";
 import StoreProvider from "./StoreProvider";
 import Datahandler from "./components/Datahandler";
@@ -12,6 +13,12 @@ export const metadata: Metadata = {
   title: "GUSTAVSSON DEV | Home",
   description: "A portfolio website for Gustavsson Development Studio, Specialising in web development and integration.",
   keywords: ["frontend", "web development", "react", "tailwindcss", "nextjs", "upwork", "freelancer", "portfolio", "gustavsson dev","mjukvara", "mjukvaruutvecklare"],
+  icons: [
+    {
+      rel: "icon",
+      url: "/favicon.ico",
+    },
+  ],
 };
 
 export default function RootLayout({
@@ -21,6 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Analytics />
       <StoreProvider>
       <body className={`${inter.className} text-white scroll-smooth`}>
         <Datahandler />
