c1ab82f87ff8bbaee3ea0e8a0214423d64ced103
Simon Gustavsson <simgus1234@gmail.com>
2024-05-13 22:22:30 +0200

    Fixed a thing

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -130,6 +130,12 @@ const ProjectPage = () => {
 
         </section>
 
+        <Link className="group mt-8 inline-flex items-center justify-center text-lg transition-colors duration-300 hover:text-purp focus-visible:text-purp" href={"/#projects"}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" data-slot="icon" className="me-2 size-6 transition-transform duration-300 group-hover:-translate-x-1 group-focus-visible:-translate-x-1 lg:h-7 lg:w-7"><path fillRule="evenodd" d="M7.28 7.72a.75.75 0 0 1 0 1.06l-2.47 2.47H21a.75.75 0 0 1 0 1.5H4.81l2.47 2.47a.75.75 0 1 1-1.06 1.06l-3.75-3.75a.75.75 0 0 1 0-1.06l3.75-3.75a.75.75 0 0 1 1.06 0Z" clipRule="evenodd"></path></svg>
+        Back to projects
+
+        </Link>
+
 
     </section>}
     </Pagewrapper>
