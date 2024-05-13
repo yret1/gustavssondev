f8fff111354c64f085680a08c8b9c8bbedf3445a
Simon Gustavsson <simgus1234@gmail.com>
2024-05-13 10:51:44 +0200

    Push

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -14,6 +14,8 @@ const Projects = () => {
 
     <Tag text="Projects" id="Projects" />
 
+    <h1 className="font-bold text-white text-xl pt-4 lg:text-5xl">Check out some of my work</h1>
+
     <select name="projectselect" className="w-40 bg-transparent rounded-full px-2 py-1 border-[1px] border-purp border-opacity-30 m-6 outline-none">
 
         <option value="">K</option>
