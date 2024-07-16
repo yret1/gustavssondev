6051d58217a30eb75dce75bff7f27b84df20353b
Simon Gustavsson <simgus1234@gmail.com>
2024-07-16 13:33:20 +0200

    Updated prices

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -2,8 +2,6 @@ import Checkbox from "./Checkbox";
 import PlaceOrder from "./PlaceOrder";
 import Tag from "./Tag";
 
-
-
 // I actually opted not to create components out of these cards due to the amount of unique code in each card.
 
 const Services = () => {
@@ -18,13 +16,18 @@ const Services = () => {
         <h1 className="font-bold text-white text-center text-nowrap text-lg lg:text-4xl">
           No idea is too big or too small, let&apos;s create something amazing!
         </h1>
-        <p>Together we will explore you&apos;r idea and shape it into the prodoct you need. Check out some of my services down below!</p>
+        <p>
+          Together we will explore you&apos;r idea and shape it into the prodoct
+          you need. Check out some of my services down below!
+        </p>
       </section>
 
       <section className="grid w-screen lg:grid-cols-5 gap-8 p-4 md:p-24 place-items-center">
         <a
-        aria-label="Get in touch with me for a custom web application"
-        href="#contact" className={`w-full h-full group md:col-span-2`}>
+          aria-label="Get in touch with me for a custom web application"
+          href="#contact"
+          className={`w-full h-full group md:col-span-2`}
+        >
           <article className="flex h-full w-full flex-col border-2 rounded-lg justify-between bg-[#0C1021] p-8">
             <section>
               <h3 className="mb-2 text-balance font-medium text-purp md:text-lg">
@@ -52,21 +55,23 @@ const Services = () => {
           </article>
         </a>
 
-        <a 
-        aria-label="Get in touch with me for a custom website"
-        href="#contact" className={`w-full h-full group md:col-span-3`}>
+        <a
+          aria-label="Get in touch with me for a custom website"
+          href="#contact"
+          className={`w-full h-full group md:col-span-3`}
+        >
           <article className="flex h-full w-full flex-col border-2 rounded-lg justify-between bg-[#0C1021] p-8">
             <section>
               <h3 className="mb-2 text-balance font-medium text-purp md:text-lg">
                 Website
               </h3>
               <p className="mb-2 flex text-3xl font-bold md:text-4xl">
-                From $ 500
+                From $ 300
               </p>
               <p className="mb-8 max-w-prose text-pretty text-sm text-white text-opacity-70">
                 Together we will create a modern and revenue creating website
-                for you or your company. Helping your business enter the
-                modern website age!
+                for you or your company. Helping your business enter the modern
+                website age!
               </p>
             </section>
             <section className="w-full h-full flex gap-2 flex-col justify-start items-start">
@@ -78,11 +83,11 @@ const Services = () => {
               <Checkbox text="Transparent workflow" checked={true} />
 
               <Checkbox
-                text="Hosting (including domain setup)  + $ 100"
+                text="Hosting (including domain setup)  + $ 100 (Only hosting $ 50)"
                 checked={false}
               />
 
-              <Checkbox text="Analytics   + $ 50" checked={false} />
+              <Checkbox text="Analytics   + $ 20" checked={false} />
             </section>
 
             <PlaceOrder />
@@ -105,8 +110,8 @@ const Services = () => {
               </p>
               <p className="mb-8 max-w-prose text-pretty text-sm text-white text-opacity-70">
                 For international clients I handle most of the contracts on
-                Upwork, a freelance platform. The reason being simpler and safer invoicing
-                internationally.{" "}
+                Upwork, a freelance platform. The reason being simpler and safer
+                invoicing internationally.{" "}
               </p>
             </section>
             <section className="w-full h-full flex gap-2 flex-col justify-start items-start">
@@ -129,7 +134,9 @@ const Services = () => {
         </a>
         <a
           aria-label="Get in touch with me for a custom web application"
-         href="#contact" className={`w-full h-full group md:col-span-2`}>
+          href="#contact"
+          className={`w-full h-full group md:col-span-2`}
+        >
           <article className="flex h-full w-full flex-col border-2 rounded-lg justify-between bg-[#0C1021] p-8">
             <section>
               <h3 className="mb-2 text-balance font-medium text-purp md:text-lg">
@@ -139,9 +146,10 @@ const Services = () => {
                 Custom pricing
               </p>
               <p className="mb-8 max-w-prose text-pretty text-sm text-white text-opacity-70">
-                In the case that your project doesn&apos;t fit any of the choices
-                provided or you have questions regarding what I can do for you,
-                Lets chat and we will figure the scope of your project out!
+                In the case that your project doesn&apos;t fit any of the
+                choices provided or you have questions regarding what I can do
+                for you, Lets chat and we will figure the scope of your project
+                out!
               </p>
             </section>
             <section className="w-full h-full flex gap-2 flex-col justify-start items-start">
